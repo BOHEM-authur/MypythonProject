f91cd8aedd5e1e52f1946b77cdb4ba87949cec97
BOHEM-authur <1576965454@qq.com>
2021-11-21 02:43:08 +0800

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+##介绍
+这是一个pythontest项目
+##上传地址：git@github.com:BOHEM-authur/MypythonProject.git
---
+++ testing/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/21 1:52
+# @Author  : BOHEM
+# @File    : __init__.py.py
+# @Software: PyCharm
